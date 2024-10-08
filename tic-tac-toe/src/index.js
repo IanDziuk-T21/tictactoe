3032b2ffceddc7cadb971ddd4def5cae92b6ed44
Ian <iand@Title21.com>
2024-10-08 08:12:50 -0700

    Add React

---
+++ tic-tac-toe/src/index.js
@@ -0,0 +1,11 @@
+import React, {StrictMode} from 'react';
+import * as ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
